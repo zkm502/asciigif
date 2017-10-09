ff5232ec440494392205cbd9b95588749d9a79c2
zkm502 <32631426+zkm502@users.noreply.github.com>
2017-10-09 04:27:52 -0300

    Update gifToAscii.sh

--- gifToAscii.sh
+++ gifToAscii.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
-if [ "$1" != "" ]; then
-    FRAMES=gifimgout%05d.jpg
-    GIF=$1
-    if [ "$2" != "" ]; then
-        SPEED=$2
+if [[ "$1" != "" ]]; then
+    frames="gifimgout%05d.jpg"
+    #TO DO, better parameter input
+    gif="$1"
+    if [[ "$2" != "" ]]; then
+        speed="$2"
     else
-        SPEED=0.05
+        speed="0.05"
     fi
-    if [ "$3" != "" ]; then
-        COLORS="--colors"
+    if [[ "$3" != "" ]]; then
+        colors="--colors"
     else
-        COLORS="--grayscale"
+        colors="--grayscale"
     fi
-
     
     # Split the gif / gifv into frames
-    if [ -f $VID ]; then
-      #  ffmpeg -i $VID $FRAMES > /dev/null 2>&1
-        convert -coalesce $GIF $FRAMES > /dev/null 2>&1
-    elif [ -f $GIF ]; then
-        convert -coalesce $GIF $FRAMES > /dev/null 2>&1
+    if [[ -f $vid ]]; then
+      #  ffmpeg -i $VID $FRAMES > /dev/null 2>&1 #not implemented yet
+        convert -coalesce $gif $frames > /dev/null 2>&1
+    elif [[ -f $gif ]]; then
+        convert -coalesce $gif $frames > /dev/null 2>&1
     fi
 
     #Display each frame in ASCII, sleep, and clear the screen
     for img in gifimgout*.jpg; do
-        jp2a -f $COLORS $img
-        sleep $SPEED 
+        jp2a -f $colors $img
+        sleep $speed
         clear
     done
 
